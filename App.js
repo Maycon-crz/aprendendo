2579e8a11f708e30bf4b0bae22bd1da86c810581
Maycon Nascimento de Oliveira <mayconnascimentodeoliveira23@gmail.com>
2020-08-09 17:34:59 -0300

    View e Text
    Seguindo documentação oficial

--- App.js
+++ App.js
@@ -2,20 +2,30 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Olá mundo!</Text>
+//       {/* <StatusBar style="auto" /> */}
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
+const MeuApp = () =>{
   return (
-    <View style={styles.container}>
-      <Text>Olá mundo! Auto</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex:1, justufyConten: "center", alignItems: "center"}}>
+      <Text>
+        Aprendendooo
+      </Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default MeuApp;
