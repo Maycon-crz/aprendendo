8eefb8f04d17acbda0649e93b4ad1077751369dd
Maycon Nascimento de Oliveira <mayconnascimentodeoliveira23@gmail.com>
2020-08-10 11:39:36 -0300

    styles e botoes

--- App.js
+++ App.js
@@ -19,22 +19,85 @@
 // });
 import { StatusBar } from 'expo-status-bar';
 import React, {useState} from 'react';
-import { StyleSheet, Text, View, ScrollView, TextInput, Image, Button } from 'react-native';
+import { 
+  StyleSheet, Text, View, ScrollView, 
+  TextInput, Image, Button, Platform,
+  TouchableHighlight, TouchableOpacity, 
+  TouchableNativeFeedback, TouchableWithoutFeedback
+} from 'react-native';
 
+const Botoes = () => {
+  return (
+    <View style={styles.container}>
+      <TouchableHighlight 
+        onPress={() => {alertando()}}
+        underlayColor="white"
+      >
+        <View style={styles.button}>
+          <Text style={styles.buttonText}>TouchableHighlight</Text>
+        </View>
+      </TouchableHighlight>
+      <TouchableOpacity onPress={() => {alertando()}} >
+        <View style={styles.button}>
+          <Text style={styles.buttonText}>TouchableOpacity</Text>
+        </View>
+      </TouchableOpacity>
+      <TouchableNativeFeedback 
+        onPress={()=>{alertando()}}
+        background={Platform.OS === 'android' ? TouchableNativeFeedback.SelectableBackground() : ''}
+      >
+        <View style={styles.button}>
+          <Text style={styles.buttonText}>TouchableNativeFeedback {Platform.OS !== 'android' ? '(Android only)' : ''}</Text>          
+        </View>
+      </TouchableNativeFeedback>
+      <TouchableWithoutFeedback
+        onPress={()=>{alertando()}}
+      >
+        <View style={styles.button}>
+          <Text style={styles.buttonText}>TouchableWithoutFeedback</Text>
+        </View>
+      </TouchableWithoutFeedback>
+      <TouchableHighlight onLongPress={()=>{alertando()}} underlayColor="white">
+        <View style={styles.button}>
+          <Text style={styles.buttonText}>Touchable with Long Press</Text>
+        </View>
+      </TouchableHighlight>
+    </View>
+  );
+}
+
+const PizzaTranslator = () => {
+  const [text, setText] = useState('');
+    return (
+      <View style={{padding: 10}}>
+        <TextInput
+          style={{height: 40}}
+          placeholder="Digite aqui para traduzir!"
+          onChangeText={text => setText(text)}
+          defaultValue={text}
+        />
+        <Text style={{padding:10, fontSize:42}}>
+          {text.split(' ').map((word) => word && '🍕').join(' ')}
+        </Text>
+      </View>
+    );
+}
+const alertando = () => {
+  alert("Bora la!")
+}
 const CatDois = (props) => {
-  const [isHungry, setIsHungry] = useState
-  (true);
+  const [estaFaminto, setEstaFaminto] = useState(true);//([primeiro parametro = Variavel de estado, segundo parametro = funcao que define o valor dessa variavel])
   return(
     <View>
       <Text>
-        Eu sou {props.name}, E eu estou {isHungry ? "hungry" : "full"}!
+        Eu sou {props.name}, E eu estou {estaFaminto ? "FAMINTO" : "CHEIO"}!
       </Text>
       <Button
         onPress={() =>{
-          setIsHungry(false);
+          setEstaFaminto(false);
         }}
-        disabled={!isHungry}
-        title={isHungry ? "Pour me some milk, please!" : "Thank you"}
+        disabled={!estaFaminto}
+        title={estaFaminto ? "Me de um pouco de Leite, por favor!" : "Obrigado!"}
       />
     </View>
   );
@@ -67,7 +130,7 @@ const MeuApp = () =>{
     <ScrollView>
       <View style={{ 
         flex:1, 
-        justufyConten: "center", 
+        justifyContent: "center", 
         alignItems: "center"
       }}>
         <Text>          
@@ -92,13 +155,48 @@ const MeuApp = () =>{
       </View>
       <View style={{ 
         flex:1, 
-        justufyConten: "center", 
+        justifyContent: "center", 
         alignItems: "center"
       }}>
-        <CatDois name="Spot" />
+        <CatDois name="Miau" />
+        <CatDois name="Tom" />
       </View>
+      <><PizzaTranslator /></>
+      <View style={styles.container}>
+        <Button
+          onPress={() => {
+            alert('Você apertou no botão!');
+          }}
+          title="Me presione!"
+        />
+        <Button
+          onPress={() => {
+            alertando()
+          }}
+          title="funcao Alerta"
+          color="#841584"
+        />
+      </View>
+      <Botoes/>
     </ScrollView>
   );
 }
-//Parei no Docs - State - useState
+const styles = StyleSheet.create({//Parecido com css
+  container:{
+    flex:1, 
+    justifyContent: "center", 
+    alignItems: "center"
+  },
+  button: {
+    marginBottom: 30,
+    width: 260,
+    alignItems: "center",
+    backgroundColor: "#2196F3"
+  },
+  buttonText:{
+    textAlign: "center",
+    padding: 20,
+    color: "white"
+  }
+});
 export default MeuApp;
